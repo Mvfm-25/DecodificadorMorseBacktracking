40f4f00d859262e7a81b15cca838466f9db3cbae
Mvfm <mvfm.filho@hotmail.com>
2025-10-31 16:38:33 -0300

    Primeiro commit, primeira implementção.

---
+++ main.py
@@ -0,0 +1,48 @@
+# Trabalho de POA
+# [mvfm]
+
+# C: 21/10/25 - A: 27/10/25
+
+MORSE_DICT = {
+    "*-": "A", "-***": "B", "-*-*": "C", "-**": "D", "*": "E",
+    "**-*": "F", "--*": "G", "****": "H", "**": "I", "*---": "J",
+    "-*-": "K", "*-**": "L", "--": "M", "-*": "N", "---": "O",
+    "*--*": "P", "--*-": "Q", "*-*": "R", "***": "S", "-": "T",
+    "**-": "U", "***-": "V", "*--": "W", "-**-": "X", "-*--": "Y", "--**": "Z",
+
+    " ": " ",
+
+    "*----": "1", "**---": "2", "***--": "3", "****-": "4", "*****": "5",
+    "-****": "6", "--***": "7", "---**": "8", "----*": "9", "-----": "0",
+
+    "*-*-*-": ".", "--**--": ",", "**--**": "?", "*----*": "'",
+    "-*-*--": "!", "-**-*":  "/", "-*--*": "(", "-*--*-": ")",
+    "*-***": "&", "---***": ":", "-*-*-*": ";", "-***-": "=",
+    "*-*-*": "+", "-****-": "-", "**--*-": "_", "*-**-*": "\"",
+    "***-**-": "$", "*--*-*": "@"
+}
+
+def decodificar_morse(morse):
+    resultados = []
+
+    def backtrack(restante, atual):
+        if not restante:
+            resultados.append("".join(atual))
+            return
+
+        for i in range(1, len(restante) + 1):
+            prefixo = restante[:i]
+            if prefixo in MORSE_DICT:
+                backtrack(restante[i:], atual + [MORSE_DICT[prefixo].lower()])
+
+    backtrack(morse, [])
+    return resultados
+
+
+# Exemplo:
+print("Decodificador Morse - v.1\n[mvfm]\n")
+entrada = input("Escreva uma mensagem em morse para ser decodificada :")
+
+possibilidades = decodificar_morse(entrada)
+
+print(possibilidades)
